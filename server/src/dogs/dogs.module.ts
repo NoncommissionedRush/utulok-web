275402ebd4b3b0b709941eaaaf0894a61484ee8d
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-19 12:06:49 +0200

    register typeorm module

--- server/src/dogs/dogs.module.ts
+++ server/src/dogs/dogs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DogsService } from './dogs.service';
 import { DogsController } from './dogs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DogEntity } from 'src/entities/dog.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DogEntity])],
   providers: [DogsService],
   controllers: [DogsController]
 })
