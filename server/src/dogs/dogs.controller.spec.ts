34497c160ef11832fafd23d95aae4e97b333884d
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-19 12:36:57 +0200

    add update endpoint

--- server/src/dogs/dogs.controller.spec.ts
+++ server/src/dogs/dogs.controller.spec.ts
@@ -1,25 +1,74 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { DogsController } from './dogs.controller';
-import { DogsService } from './dogs.service';
-import { MockProvider } from 'src/@types';
+import { Test, TestingModule } from "@nestjs/testing";
+import { DogsController } from "./dogs.controller";
+import { DogsService } from "./dogs.service";
+import { MockProvider } from "src/@types";
+import { CreateDogDto } from "src/dtos/create-dog.dto";
+import { DogsFilter } from "src/dtos/dogs-filter.dto";
+import { UpdateDogDto } from "src/dtos/update-dog.dto";
 
-describe('DogsController', () => {
+describe("DogsController", () => {
   let controller: DogsController;
   let dogsService: MockProvider<DogsService>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DogsController],
-    }).useMocker(token => {
-       if(token === DogsService){
-          return {}
-        } 
-      }).compile();
+    }).useMocker((token) => {
+      if (token === DogsService) {
+        return {
+          create: jest.fn(),
+          get: jest.fn(),
+          getById: jest.fn(),
+          update: jest.fn(),
+        };
+      }
+    }).compile();
 
     controller = module.get<DogsController>(DogsController);
     dogsService = module.get(DogsService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("createDog", () => {
+    const dto = {} as CreateDogDto;
+
+    it("calls create on dogsService", async () => {
+      await controller.createDog(dto);
+
+      expect(dogsService.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe("getDogs", () => {
+    const query = {} as DogsFilter;
+
+    it("calls get on dogsService", async () => {
+      await controller.getDogs(query);
+
+      expect(dogsService.get).toHaveBeenCalledWith(query);
+    });
+  });
+
+  describe("getDog", () => {
+    const id = 1;
+
+    it("calls getById on dogsService", async () => {
+      await controller.getDog(id);
+
+      expect(dogsService.getById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe("update", () => {
+    const id = 1;
+    const dto = {} as UpdateDogDto;
+
+    it("calls update on dogsService", async () => {
+      await controller.update(id, dto);
+
+      expect(dogsService.update).toHaveBeenCalledWith(id, dto);
+    });
   });
 });
--- server/src/dogs/dogs.controller.ts
+++ server/src/dogs/dogs.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Param, Post, Query, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from "@nestjs/common";
 import { CreateDogDto } from "../dtos/create-dog.dto";
 import { DogsService } from "./dogs.service";
 import { DogsFilter } from "src/dtos/dogs-filter.dto";
 import { AuthGuard } from "src/guards/auth.guard";
+import { UpdateDogDto } from "src/dtos/update-dog.dto";
 
 @Controller("dogs")
 export class DogsController {
@@ -18,11 +27,17 @@ export class DogsController {
 
   @Get("/")
   async getDogs(@Query() query: DogsFilter) {
-    return this.dogsService.get(query)
+    return this.dogsService.get(query);
   }
 
-  @Get('/:id')
-  async getDog(@Param('id') id: number) {
-    return this.dogsService.getById((id)) 
+  @Get("/:id")
+  async getDog(@Param("id") id: number) {
+    return this.dogsService.getById(id);
+  }
+
+  @Put("/:id")
+  @UseGuards(AuthGuard)
+  async update(@Param("id") id: number, @Query() query: UpdateDogDto) {
+    return this.dogsService.update(id, query);
   }
 }
