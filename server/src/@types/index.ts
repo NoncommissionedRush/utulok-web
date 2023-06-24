3159b2fa84e774c593d7580c553a87a2982a2b74
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-24 19:59:40 +0200

    add test files

--- server/src/@types/index.ts
+++ server/src/@types/index.ts
@@ -1,3 +1,13 @@
+import { AbstractAdoptionEntity } from "../entities/abstract.adoption";
+import { DogEntity } from "../entities/dog.entity";
+
 export type MockProvider<T> = {
     [P in keyof T]: jest.Mock<any>;
 };
+
+export interface Adoption extends AbstractAdoptionEntity {
+    dog: DogEntity
+}
+
+
+
