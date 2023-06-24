f0b5ddf70bf880c0ed3a07fe63b680fe75019861
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-24 19:57:30 +0200

    integrate adoption service

--- server/src/dogs/dogs.controller.ts
+++ server/src/dogs/dogs.controller.ts
@@ -14,13 +14,15 @@ import { DogsFilter } from "src/dtos/dogs-filter.dto";
 import { AuthGuard } from "src/guards/auth.guard";
 import { UpdateDogDto } from "src/dtos/update-dog.dto";
 import { AdoptDogDto } from "src/dtos/adopt-dog.dto";
-import { Status } from "src/entities/dog.entity";
 import { AdminDogsFilter } from "src/dtos/adming-dogs-filter.dto";
+import { AdoptionStatus } from "../entities/abstract.adoption";
+import { AdoptionService } from "../adoption/adoption.service";
 
 @Controller("dogs")
 export class DogsController {
   constructor(
     private readonly dogsService: DogsService,
+    private readonly adoptionService: AdoptionService
   ) {}
 
   @Post("/create")
@@ -31,8 +33,21 @@ export class DogsController {
 
   @Post("/adopt")
   async adopt(@Query() query: AdoptDogDto) {
-    //TODO: send email
-    return this.dogsService.update(query.dogId, { status: Status.RESERVED });
+    //TODO: send email to admin
+    return this.adoptionService.createAdoption(query);
+  }
+
+  @Post("process-adoption")
+  @UseGuards(AuthGuard)
+  async processAdoption(@Query("id") id: number, @Query("status") status: AdoptionStatus) {
+    //TODO: send email to adopter
+    this.adoptionService.processAdoption(id, status);
+  }
+
+  @Get("/pending-adoptions")
+  @UseGuards(AuthGuard)
+  async getPendingAdoptions() {
+    return this.adoptionService.getPendingAdoptions();
   }
 
   @Get("/")
