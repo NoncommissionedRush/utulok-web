8954581fbe13f56a5365fd54186619bf55f77377
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-25 22:01:47 +0200

    add listener on uncaught exceptions

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
+  process.on('uncaughtException', e => {
+    console.error(e);
+  });
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
