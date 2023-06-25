6943f83a4806990f50abe28b1b68baf037f24f34
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-25 10:03:25 +0200

    refactor and add event emitter listeners

--- server/src/adoption/adoption.repository.ts
+++ server/src/adoption/adoption.repository.ts
@@ -1,13 +1,15 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { StandardAdoption } from '../entities/standard-adoption.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOperator, FindOptionsWhere, Repository } from 'typeorm';
 import { VirtualAdoption } from '../entities/virtual-adoption.entity';
 import { TemporaryAdoption } from '../entities/temporary-adoption.entity';
 import { AdoptDogDto } from '../dtos/adopt-dog.dto';
 import { Adoption } from '../@types';
 import { AdoptionStatus, AdoptionType } from '../entities/abstract.adoption';
 import { DogEntity } from '../entities/dog.entity';
+import { OnEvent } from '@nestjs/event-emitter';
+import { Events } from '../events';
 
 @Injectable()
 export class AdoptionRepository {
@@ -21,9 +23,33 @@ export class AdoptionRepository {
     ) { }
 
     async createAdoption(dto: AdoptDogDto, dog?: DogEntity): Promise<Adoption> {
-        let repository: Repository<any>;
+        const repository = this.getRepository(dto.type);
 
-        switch (dto.type) {
+        const newAdoption = repository.create({
+            type: dto.type,
+            adopterEmail: dto.email,
+            adopterPhone: dto.phone,
+        })
+
+        if (dto.type === AdoptionType.VIRTUAL) {
+            newAdoption.status = AdoptionStatus.APPROVED;
+        }
+
+        newAdoption.dog = dog;
+
+        return repository.save(newAdoption);
+    }
+
+    async findOneBy(options: FindOptionsWhere<Adoption>): Promise<Adoption> {
+        const repository = this.getRepository(options.type);
+
+        return repository.findOne({ where: options})
+    }
+
+    private getRepository(type: AdoptionType | FindOperator<AdoptionType>) {
+        let repository: Repository<Adoption>;
+
+        switch (type) {
             case AdoptionType.STANDARD:
                 repository = this.standardAdoptionRepository;
                 break;
@@ -36,50 +62,7 @@ export class AdoptionRepository {
             default:
                 throw new BadRequestException('Invalid adoption type');
         }
-
-        const newAdoption = repository.create({
-            type: dto.type,
-            adopterEmail: dto.email,
-            adopterPhone: dto.phone,
-        })
-
-        if(dto.type === AdoptionType.VIRTUAL) {
-            newAdoption.status = AdoptionStatus.APPROVED;
-        }
-
-        newAdoption.dog = dog;
-
-        return repository.save(newAdoption);
-    }
-
-    //TODO: id can be same between repositories, fix this
-    async get(id: number): Promise<Adoption> {
-        const standardAdoption = await this.standardAdoptionRepository.findOne({ where: { id } });
-        if (standardAdoption) return standardAdoption;
-
-        const virtualAdoption = await this.virtualAdoptionRepository.findOne({ where: { id } });
-        if (virtualAdoption) return virtualAdoption;
-
-        const temporaryAdoption = await this.temporaryAdoptionRepository.findOne({ where: { id } });
-        if (temporaryAdoption) return temporaryAdoption;
-    }
-
-    async update(id: number, data: Partial<Adoption>): Promise<Adoption> {
-        const adoption = await this.get(id);
-
-        return this.standardAdoptionRepository.save({ ...adoption, ...data });
-    }
-
-    async delete(id: number): Promise<void> {
-        await this.standardAdoptionRepository.delete(id);
-    }
-
-    async deleteTemporaryAdoptions(dogId: number) {
-        await this.temporaryAdoptionRepository.delete({ dog: { id: dogId } });
-    }
-
-    async deleteVirtualAdoptions(dogId: number) {
-        await this.virtualAdoptionRepository.delete({ dog: { id: dogId } });
+        return repository;
     }
 
     async getPendingAdoptions(): Promise<Adoption[]> {
@@ -87,5 +70,34 @@ export class AdoptionRepository {
         const temporaryAdoptions = await this.temporaryAdoptionRepository.find({ where: { status: AdoptionStatus.PENDING } });
 
         return [...standardAdoptions, ...temporaryAdoptions]
+    }
+
+    async setAdoptionStatus(adoption: Adoption, status: AdoptionStatus) {
+        const repository = this.getRepository(adoption.type);
+
+        return repository.save({ ...adoption, status })
+    }
+
+    async remove(adoption: Adoption) {
+        const repository = this.getRepository(adoption.type);
+
+        return repository.remove(adoption);
+    }
+
+    @OnEvent(Events.ADOPTION_STANDARD_APPROVED)
+    async onStandardAdoptionApproved(adoption: StandardAdoption) {
+        this.setAdoptionStatus(adoption, AdoptionStatus.APPROVED);
+
+        this.deleteTemporaryAdoptions(adoption.dog.id);
+
+        this.deleteVirtualAdoptions(adoption.dog.id);
+    }
+
+    private async deleteTemporaryAdoptions(dogId: number) {
+        this.temporaryAdoptionRepository.delete({ dog: { id: dogId } });
+    }
+
+    private async deleteVirtualAdoptions(dogId: number) {
+        this.virtualAdoptionRepository.delete({ dog: { id: dogId } });
     }
 }
