74ce42c2a0a7b5831bd115f391c9be7f655e852f
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-06-25 07:31:04 +0200

    add serializer, replace query with body

--- server/src/dogs/dogs.controller.ts
+++ server/src/dogs/dogs.controller.ts
@@ -1,4 +1,6 @@
 import {
+  Body,
+  ClassSerializerInterceptor,
   Controller,
   Get,
   Param,
@@ -7,6 +9,7 @@ import {
   Query,
   Session,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
 import { CreateDogDto } from "../dtos/create-dog.dto";
 import { DogsService } from "./dogs.service";
@@ -19,6 +22,7 @@ import { AdoptionStatus } from "../entities/abstract.adoption";
 import { AdoptionService } from "../adoption/adoption.service";
 
 @Controller("dogs")
+@UseInterceptors(ClassSerializerInterceptor)
 export class DogsController {
   constructor(
     private readonly dogsService: DogsService,
@@ -27,16 +31,17 @@ export class DogsController {
 
   @Post("/create")
   @UseGuards(AuthGuard)
-  async createDog(dto: CreateDogDto) {
+  async createDog(@Body() dto: CreateDogDto) {
     return this.dogsService.create(dto);
   }
 
   @Post("/adopt")
-  async adopt(@Query() query: AdoptDogDto) {
+  async adopt(@Body() query: AdoptDogDto) {
     //TODO: send email to admin
     return this.adoptionService.createAdoption(query);
   }
 
+  //TODO: fix validation on incoming query
   @Post("process-adoption")
   @UseGuards(AuthGuard)
   async processAdoption(@Query("id") id: number, @Query("status") status: AdoptionStatus) {
@@ -68,7 +73,7 @@ export class DogsController {
 
   @Put("/:id")
   @UseGuards(AuthGuard)
-  async update(@Param("id") id: number, @Query() query: UpdateDogDto) {
+  async update(@Param("id") id: number, @Body() query: UpdateDogDto) {
     return this.dogsService.update(id, query);
   }
 }
