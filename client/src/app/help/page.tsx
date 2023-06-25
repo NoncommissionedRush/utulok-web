a4586ff00de9cd603878f7830d9d3689e48d9199
luboszavarsky <zavarsky.lubos@gmail.com>
2023-06-25 14:09:59 +0200

    add basic routes and layout

--- client/src/app/help/page.tsx
+++ client/src/app/help/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Help() {
   return (
     <section className="min-h-screen w-full scroll-mt-9 bg-gray-200">
       <div className="max-w-2xl p-3 flex flex-col">
-        <h1 className="text-2xl font-bold">Domov</h1>
+        <h1 className="text-2xl font-bold">Ako pomôcť</h1>
       </div>
     </section>
   );
