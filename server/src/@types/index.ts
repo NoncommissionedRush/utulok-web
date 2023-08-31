11df70aea6abde0fec48b0e74a42622af4d564c1
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2023-08-31 16:13:10 +0200

    add types directory

--- server/src/@types/index.ts
+++ server/src/@types/index.ts
@@ -1,38 +1,3 @@
-import { AbstractAdoptionEntity } from "../entities/abstract.adoption";
-import { Age, DogEntity, DogStatus, EligibleFor, Sex, Size } from "../entities/dog.entity";
-
-export type MockProvider<T> = {
-    [P in keyof T]: jest.Mock<any>;
-};
-
-export interface Adoption extends AbstractAdoptionEntity {
-    dog: DogEntity
-}
-
-export interface Dog {
-  name: string;
-
-  size: Size;
-
-  sex: Sex;
-
-  age: Age;
-
-  status: DogStatus;
-
-  eligibleFor: EligibleFor;
-
-  breed: string;
-
-  color: string;
-
-  isKidFriendly: boolean;
-
-  isVaccinated: boolean;
-
-  isCastrated: boolean;
-}
-
 
 
 
